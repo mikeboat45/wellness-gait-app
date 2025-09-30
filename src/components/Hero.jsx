d88f24423023d9f493afeb0cbf5b000c86a2c941
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-09-30 21:17:55 +0000

    add background image for image section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import Button from "./Button";
+import bgImage from "../assets/gait-app-bg.jpg";
 
 export default function Hero() {
   return (
-    <section className="py-0.5">
-      <div className="flex flex-col items-center py-30 bg-blue-50">
+    <section className="bg-cover bg-center">
+      <div
+        className="flex flex-col items-center py-70 bg-blue-50 bg-cover bg-center w-full h-screen "
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
         <h2 className="text-4xl font-bold">Move Freely, Live Fully</h2>
         <p className="italic text-xl py-1">
           Your journey to recovery starts with us!
