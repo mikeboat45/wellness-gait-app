e02a8d1f5982dcabb19274569628d354326745ff
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-10-01 13:02:29 +0000

    feat: appointment request for users not logged in

---
+++ src/components/forms/PatientIntake.jsx
@@ -0,0 +1,56 @@
+{
+  /*from book appointment button */
+}
+
+import formBgImg from "../../assets/intake-form-bg.jpg";
+import Button from "../landing-page/Button";
+
+export default function PatientIntakeForm() {
+  return (
+    <div
+      className="bg-cover bg-center flex justify-center items-center w-full h-screen"
+      style={{ backgroundImage: `url(${formBgImg})` }}
+    >
+      <div className="bg-teal-700/30 text-white w-1/2 h-3/4 rounded-xl ">
+        <form action="" className="flex flex-col gap-4 p-4">
+          <h2 className="font-semibold text-black text-xl">
+            Request an appointment
+          </h2>
+          <label htmlFor="full-name">Full Name: </label>
+          <input
+            type="text"
+            id="full-name"
+            name="full-name"
+            className="bg-black/70 h-10 text-white rounded-md pl-2"
+            placeholder="Jay Mayors"
+          />
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="bg-black/70 h-10 text-white rounded-md pl-2"
+            placeholder="jay.may45@gmail.com"
+          />
+          <label htmlFor="complaint">Complaint: </label>
+          <input
+            type="text"
+            id="complaint"
+            name="complaint"
+            placeholder="e.g. Knee pain for the past two days"
+            required
+            className="bg-black/70 h-10 text-white rounded-md pl-2 "
+          />
+          <label htmlFor="history">Describe your problem</label>
+          <textarea
+            name="history"
+            id="history"
+            placeholder="Write a full description of your complaint..."
+            className="bg-black/70 h-24 text-white rounded-md pl-2"
+          ></textarea>
+          <Button text="Submit" /> {/* to signup form */}
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import LoginForm from "./components/forms/LoginForm";
+// import LoginForm from "./components/forms/LoginForm";
 // import SignUp from "./components/forms/SignUpForm";
+import PatientForm from "./components/forms/PatientIntake";
 // import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LoginForm />
+    <PatientForm />
   </StrictMode>
 );
