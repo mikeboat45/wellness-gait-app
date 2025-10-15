2c88e26c9029ff5a27d65df4b3dff512553d6524
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-10-15 12:35:56 +0000

    fix: adjust service card onHover display

--- src/components/landing-page/ServiceCard.jsx
+++ src/components/landing-page/ServiceCard.jsx
@@ -15,10 +15,14 @@ export default function ServiceCard(props) {
     <div
       onMouseEnter={showOnHover}
       onMouseLeave={clearOnHover}
-      className="bg-teal-700 text-white w-full h-48 py-4 sm:py-8 text-center hover:bg-teal-900 hover:shadow-teal-800 rounded shadow-md shadow-teal-600"
+      className="bg-teal-700 text-white w-full h-48 p-4 sm:py-8 text-center hover:bg-teal-900 hover:shadow-teal-800 rounded shadow-md shadow-teal-600"
     >
-      <h3 className="text-lg sm:text-xl font-bold">{props.title}</h3>
-      {isHovered && <p className="p-2 sm:p-4 text-base sm:text-lg">{props.text}</p>}
+      {!isHovered && (
+        <h3 className="text-lg sm:text-xl font-bold">{props.title}</h3>
+      )}
+      {isHovered && (
+        <p className="p-2 sm:p-4 text-base sm:text-lg">{props.text}</p>
+      )}
     </div>
   );
 }
