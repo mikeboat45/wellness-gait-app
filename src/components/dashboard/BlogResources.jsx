a0ea9aae68787dbb24c5ba95af517d43e1390804
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-10-17 08:40:37 +0000

    refactor: changes to data displayed in the resources section

--- src/components/dashboard/BlogResources.jsx
+++ src/components/dashboard/BlogResources.jsx
@@ -1,13 +1,13 @@
 export default function BlogResources(props) {
   return (
     <>
-      <div className="rounded-2xl flex flex-col h-72 bg-blue-100 shadow-md shadow-blue-200">
+      <div className="rounded-2xl flex flex-col h-84 bg-blue-100 shadow-md shadow-blue-200">
         <img
           src={props.img.src}
           alt={props.img.alt}
           className="h-36 rounded-2xl block"
         />
-        <h4 className="px-4 py-2">{props.title}</h4>
+        <h4 className="px-4 py-2 text-center font-semibold">{props.title}</h4>
         <p className="px-4 ">{props.info}</p>
       </div>
     </>
--- src/components/dashboard/VideoResources.jsx
+++ src/components/dashboard/VideoResources.jsx
@@ -1,25 +1,13 @@
 export default function VideoResources(props) {
   return (
-    <div className="rounded-2xl flex flex-col h-72 bg-blue-100 shadow-md shadow-blue-200 ">
+    <div className="rounded-2xl flex flex-col h-84 bg-blue-100 shadow-md shadow-blue-200 ">
       <img
         src={props.img.src}
         alt={props.img.alt}
         className="h-36 rounded-2xl block"
       />
-      <h4 className="px-4 py-2">{props.title}</h4>
+      <h4 className="px-4 py-2 text-center font-semibold">{props.title}</h4>
       <p className="px-4 ">{props.info}</p>
     </div>
   );
-}
-
-{
-  /* <div className="border-2 border-teal-800  rounded-2xl flex flex-col h-72  ">
-      <img
-        src={props.img.src}
-        alt={props.img.alt}
-        className="h-36 rounded-2xl block m-2"
-      />
-      <h4 className="p-4 ">{props.title}</h4>
-      <p className="p-4 ">{props.info}</p>
-    </div> */
 }
